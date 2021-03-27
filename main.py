38952a8bb6160378b9e95b24bdbc89e5fa344336
deripipka <mapodshivalov@gmail.com>
2021-03-27 20:38:39 +0300

    Create main1.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-
-
 print('hello world')
+
+Mylist = [1, 3, 4, 6, 8, 6, 4, 5, 6, 7, 5, 3, 3]
+
+x = [x**2 for x in Mylist]
+print(x)
--- main1.py
+++ main1.py
@@ -1,2 +1,11 @@
 print('hello world')
 print('hello, github')
+
+
+x = int(input('Введите целое число: '))
+y = 1
+for i in range(1, x+1):
+    y = y * i
+print(y)
+
+
